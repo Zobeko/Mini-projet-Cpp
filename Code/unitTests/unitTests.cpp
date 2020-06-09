d1fde40aee71ce3f643123fb75334d31ff062c52
Louis <louis.ginesy@telecom-sudparis.eu>
2020-06-09 20:41:47 +0200

    Ajout d'un test pour vérifier l'intersection entre une unite et un pickups

--- Code/unitTests/unitTests.cpp
+++ Code/unitTests/unitTests.cpp
@@ -1,9 +1,19 @@
 #include <gtest/gtest.h>
+#include "PickUp.h"
+#include "Unite.h"
 
 
 namespace myNameSpace {
 
-TEST(TestReadXML, TestCircle) {    
+TEST(TestCreationUnite, TestIntersectPickUp) {
+    std::map<std::string, sf::Texture> textDictionnary;
+
+    PickUp pickupTest(0,0,32,32,"Pierre.png",textDictionnary);
+    Unite UniteTest1(10, 0, 32, 32, "Pierre.png", textDictionnary);
+    Unite UniteTest2(100, 0, 32, 32, "Pierre.png", textDictionnary);
+
+    EXPECT_TRUE(pickupTest.intersect(UniteTest1));
+    EXPECT_FALSE(pickupTest.intersect(UniteTest2));
     EXPECT_EQ(1,1);
 }
 
